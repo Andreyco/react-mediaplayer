4a62170749ac84ca58a43895f8ae8914ca0bf1c9
Andrej Badin <contact@andrejbadin.com>
2015-07-05 16:23:31 +0200

    Add volume indicator.

--- src/ui/Controls.js
+++ src/ui/Controls.js
@@ -26,10 +26,27 @@ module.exports = React.createClass({
     this.props.requestVolumeChange(parseFloat(event.target.value));
   },
 
+  renderVolumeControls() {
+    const classnames = ['videoplayer--volume-indicator'];
+    if (this.props.volume === 0 || this.props.muted) {
+      classnames.push('level-muted')
+    } else if (this.props.volume < 0.34) {
+      classnames.push('level-1-3');
+    } else if (this.props.volume < 0.67) {
+      classnames.push('level-2-3');
+    } else {
+      classnames.push('level-3-3');
+    }
+
+    return (<div className="videoplayer--volumne-controls">
+      <div className={classnames.join(' ')} onClick={this.props.requestMute}>{this.props.muted?'Muted':'Mute'}</div>
+      <input type="range" max="1" min="0" step={0.01} value={this.props.volume} onChange={this._requestVolumeChange} />
+    </div>);
+  },
+
   render() {
     return (<div>
-      <div onClick={this.props.requestMute}>{this.props.muted?'Muted':'Mute'}</div>
-      <input type="range" max="1" min="0" step={0.01} value={this.props.volume} onChange={this._requestVolumeChange} />
+      { this.renderVolumeControls() }
     </div>)
   },
 });
